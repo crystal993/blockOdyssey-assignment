07a0f3869fe1aa7bb69b12fcb204250ae5acc39f
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2023-01-17 08:32:39 +0900

    ⭐ feat : 리덕스 툴킷 설정

---
+++ src/redux/modules/adminSlice.js
@@ -0,0 +1,17 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+};
+
+export const adminSlice = createSlice({
+  name: 'admin',
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+// Action creators are generated for each case reducer function
+export const {} = adminSlice.actions;
+
+export default adminSlice.reducer;
