8e34d52859c7205b6904cce8397a86fa12c63112
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2023-01-17 10:47:40 +0900

    ⭐ feat : 공통 컴포넌트 Input 제작 - onChange, placeholder, required, value, label 추가 - input css

--- src/component/elements/input/Input.jsx
+++ src/component/elements/input/Input.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from './Input.module.css';
 
-const Input = () => {
+const Input = (props) => {
+  const {onChange, placeholder, required, value, label = ''} = props;
   return (
-    <div>
-      <input></input>
+    <div className={`${styled.wrapper}`}>
+      <label>{label}</label>
+      <input
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+        required={required}
+      />
     </div>
   );
 };
