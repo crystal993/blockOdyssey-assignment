a9b57c4f6da8475d3375349aa870c006dc1a431f
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2023-01-17 09:49:25 +0900

    ⭐ feat : main페이지 SearchSection 컴포넌트 CSS - input, button, select 위치 조정 - searchSection CSS 작업

---
+++ src/component/elements/select/Select.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Select = () => {
+  return (
+    <div>
+      <select></select>
+    </div>
+  );
+};
+
+export default Select;
--- src/component/main/searchSection/SearchSection.jsx
+++ src/component/main/searchSection/SearchSection.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import styled from './SearchSection.module.css';
+import Input from '../../elements/input/Input';
+import Select from '../../elements/select/Select';
+import Button from '../../elements/button/Button';
 
 const SearchSection = () => {
-  return <div>SearchSection</div>;
+  return (
+    <div className={styled.wrapper}>
+      <div className={styled.upper_wrapper}>
+        <h3 className={styled.title}>상품 검색</h3>
+      </div>
+      <div className={styled.lower_wrapper}>
+        <h3 className={styled.title}>검색</h3>
+        <div className={styled.search_wrapper}>
+          <Select />
+          <Input />
+          <Button text={'조회'} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchSection;
