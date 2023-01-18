92217a1ec61ec59b5748b690149dc88d9aa9c268
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2023-01-18 11:56:06 +0900

    ⭐ feat : main 페이지 ListSection 컴포넌트 CSS - grid 사용

--- src/component/main/listSection/ListSection.jsx
+++ src/component/main/listSection/ListSection.jsx
@@ -1,8 +1,118 @@
 import React from 'react';
 import styled from './ListSection.module.css';
+import Pagination from '../pagination/Pagination';
+import ListItem from '../listItem/ListItem';
 
+// 상품번호, 상품명, 브랜드, 상품내용, 가격, 평점, 재고
 const ListSection = () => {
-  return <div>ListSection</div>;
+  return (
+    <div className={`${styled.wrapper}`}>
+      <div className={`${styled.item_wrapper}`}>
+        <div className={`${styled.item} ${styled.item_title}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+        <div className={`${styled.item}`}>
+          <p className={`${styled.text}`}>상품번호</p>
+          <p className={`${styled.text}`}>상품명</p>
+          <p className={`${styled.text}`}>브랜드</p>
+          <p className={`${styled.text}`}>상품내용</p>
+          <p className={`${styled.text}`}>가격</p>
+          <p className={`${styled.text}`}>평점</p>
+          <p className={`${styled.text}`}>재고</p>
+        </div>
+      </div>
+      <div>
+        <Pagination />
+      </div>
+    </div>
+  );
 };
 
 export default ListSection;
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from './Main.module.css';
 import SearchSection from '../../component/main/searchSection/SearchSection';
 import ListSection from '../../component/main/listSection/ListSection';
-import Pagination from '../../component/main/pagination/Pagination';
 
 const Main = () => {
   return (
@@ -14,7 +13,6 @@ const Main = () => {
         </div>
       </div>
       <ListSection />
-      <Pagination />
     </div>
   );
 };
