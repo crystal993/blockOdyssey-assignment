307d6e497c0247b7d6a27f71f2dda6ce79a7c86b
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2023-01-19 16:01:10 +0900

    👏 chore : react-query dev tool 비활성화

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+// import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
 export const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +13,7 @@ root.render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen />
+      {/* <ReactQueryDevtools initialIsOpen /> */}
     </QueryClientProvider>
   </Provider>
 );
